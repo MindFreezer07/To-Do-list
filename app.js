a5a9e952e0c99b4085e47a72c8ebfa5c2466a588
Anurag Singh <abhinavpaul54@gmail.com>
2022-10-05 00:18:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["buy food", "cook food", "eat food"];
+const workItem = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {
+    listTitle: day,
+    newItem: items
+  });
+});
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work List") {
+    workItem.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect("/");
+  };
+
+});
+
+app.get("/work", function(req, res) {
+
+  res.render("list", {
+    listTitle: "Work List",
+    newItem: workItem
+  });
+});
+
+app.post("/work", function(req, res) {
+
+  const item = req.body.newItem;
+  workItem.push(item);
+  res.redirect("/work");
+
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+});
